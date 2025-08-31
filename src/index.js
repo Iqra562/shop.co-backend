c52f22b28eee046f5da8520684bf4e87f5ca701a
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-08-31 22:58:19 +0500

    api response and error handlers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import mongoose from "mongoose";
 import {DB_NAME} from './constant.js'
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
+// import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 connectDB()
-
+.then(()=>{
+      app.on("errror",(error)=>{
+        console.log("ERR",error )
+        throw error
+    })
+    app.listen(process.env.PORT || 800,()=>{
+        console.log(`Server is running `)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo db connection failed!!! ", err)
+})
 
